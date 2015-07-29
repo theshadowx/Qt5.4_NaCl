996c14c0c53e3f8e06a28869ed84d090b3ea222a
Ali Diouri <alidiouri@gmail.com>
2015-07-29 22:33:28 +0100

    Update QtNaCl_docker.sh

--- QtNaCl_docker.sh
+++ QtNaCl_docker.sh
@@ -66,7 +66,7 @@ mv compilenacl.sh /usr/bin/compilenacl
 # Cleaning
 printf 'y' | rm -r qt5-qtdeclarative-nacl
 printf 'y' | rm -r qt5-qtbase-nacl
-printf 'y' | rm -r Qt5.4.2_src
+#printf 'y' | rm -r Qt5.4.2_src
 rm qtbase.patch
 rm tools.patch
-#rm QtNaCl_docker.sh
+rm QtNaCl_docker.sh
